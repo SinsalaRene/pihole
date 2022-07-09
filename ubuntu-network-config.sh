350a82ad93adb066c02e3447fcb764aca95a7737
Rene Fibus <renefibus@googlemail.com>
2022-07-09 17:01:23 +0200

    feat: allow local dns resolution for other containers

--- ubuntu-network-config.sh
+++ ubuntu-network-config.sh
@@ -18,3 +18,48 @@ sudo ip6tables -t nat -A PREROUTING -p udp --dport 53 -m addrtype --dst-type LOC
 sudo iptables -t nat -A PREROUTING -p tcp --dport 53 -m addrtype --dst-type LOCAL -j DOCKER
 
 sudo iptables -t nat -A PREROUTING -p udp --dport 53 -m addrtype --dst-type LOCAL -j DOCKER
+
+
+sudo iptables -t nat -A PREROUTING -p udp --dport 53 -m addrtype --dst-type LOCAL -j DOCKER --src-range 
+
+### Better network config to prevent loops if i want to use other docker containers on the device 
+# https://dxpetti.com/blog/2019/using-iptables-to-force-all-dns-queries-to-a-pi-hole/
+
+iptables -t nat -A PREROUTING -i br0 -p udp ! --source piholeaddress ! --destination piholeaddress --dport 53 -j DNAT --to piholeaddress
+ 
+iptables -t nat -A PREROUTING -i br0 -p tcp ! --source piholeaddress ! --destination piholeaddress --dport 53 -j DNAT --to piholeaddress
+
+# How to delete the previous rules?
+# Show the rule mumbers
+sudo iptables --list -vn --line-numbers
+# Or better use this as this actually shows you the preprouting chain!
+sudo iptables -t nat -L --line-numbers
+# Delete the rules from the PREROUTING Chain in nat
+iptables -t nat --delete PREROUTING 1
+
+## Actually working Rules :
+## These will allow other docker containers to still have DNS resolution!
+iptables -t nat -A PREROUTING -p udp --dport 53 -m addrtype --dst-type LOCAL ! --source 192.168.178.66  -j DOCKER
+iptables -t nat -A PREROUTING -p tcp --dport 53 -m addrtype --dst-type LOCAL ! --source 192.168.178.66  -j DOCKER
+
+## Everything below was just for experimenting!
+
+
+iptables -t nat -A PREROUTING -p udp ! --source 192.168.178.66 ! --destination 192.168.178.66 --dport 53 -j DNAT --to 192.168.178.66
+
+iptables -t nat -A PREROUTING -i br0 -p udp ! --source 172.0.0.0/8 ! --destination 172.0.0.0/8 --dport 53 -j DNAT --to 172.18.0.2
+
+iptables -t nat -A PREROUTING -i br0 -p udp ! --source 192.168.178.66 ! --destination 192.168.178.66 --dport 53 -j DOCKER --to 172.18.0.2
+ 
+iptables -t nat -A PREROUTING -i br0 -p tcp ! --source piholeaddress ! --destination piholeaddress --dport 53 -j DNAT --to piholeaddress
+
+iptables -t nat -A PREROUTING -p udp --dport 53 -m addrtype --dst-type LOCAL -j DOCKER
+iptables -t nat -A PREROUTING -p udp ! --source 192.168.178.66 ! --destination 192.168.178.66 --dport 53 -j DOCKER
+iptables -t nat -A PREROUTING -i eno1 -p udp ! --source 172.0.0.0/8 ! --destination 172.0.0.0/8 --dport 53 -j DOCKER
+
+iptables -t nat -A PREROUTING -i eno1 -p udp --source 192.168.178.66 --destination 192.168.178.66 --dport 53 -j DOCKER
+
+iptables -t nat -A PREROUTING -i eno1 -p udp --source 192.168.178.66 --destination 172.21.0.0/24 --dport 53 -j DNAT --to 172.21.0.1
+
+
+
